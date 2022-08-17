abc63b8bd7028fa42bf661c94b093ce4f9da4571
itsezc <chirub@foretag.co>
2022-08-18 00:24:29 +0100

    add(crate): palette

---
+++ packages/palette/src/lib.rs
@@ -0,0 +1,8 @@
+mod example;
+
+pub mod palette;
+pub use palette::{Palette, Theme, Token};
+
+pub fn main() {
+	println!("test");
+}
---
+++ packages/palette/src/palette.rs
@@ -0,0 +1,48 @@
+use yew::AttrValue;
+
+#[derive(Clone, PartialEq, Eq)]
+pub struct Palette<T, A> {
+	pub default: Theme<T, A>,
+	pub themes: Vec<Theme<T, A>>,
+}
+
+impl<T: Clone, A: Clone> Palette<T, A> {
+	pub fn new(themes: Vec<Theme<T, A>>) -> Self {
+		Palette {
+			default: themes.first().cloned().expect("No themes exist"),
+			themes,
+		}
+	}
+
+	pub fn mix() -> String {
+		String::from("")
+	}
+}
+
+#[derive(Clone, PartialEq, Eq)]
+pub struct Theme<T, A> {
+	pub name: String,
+	pub tokens: T,
+	pub assets: A,
+}
+
+impl<T, A> Theme<T, A> {
+	pub fn new(name: String, tokens: T, assets: A) -> Self {
+		Self {
+			name,
+			tokens,
+			assets,
+		}
+	}
+}
+
+#[derive(Default)]
+pub struct Token {
+	key: AttrValue,
+	value: AttrValue,
+}
+
+#[derive(Default)]
+pub struct Tokens {
+	colors: Vec<Token>,
+}
