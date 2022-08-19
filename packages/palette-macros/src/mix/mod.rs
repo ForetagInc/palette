a520a9a31752f3b9ec3ae906a6ad28cad433043f
KennethGomez <kennethgomezfdz@gmail.com>
2022-08-19 17:19:06 +0200

    implement: Variant parser

--- packages/palette-macros/src/mix/mod.rs
+++ packages/palette-macros/src/mix/mod.rs
@@ -2,18 +2,21 @@ use boolinator::Boolinator;
 use quote::{quote, ToTokens};
 use syn::buffer::Cursor;
 use syn::parse::{Parse, ParseStream};
-use syn::{Expr, Ident, Lit, LitStr, Token};
+use syn::{braced, Expr, Ident, Lit, LitStr, Token};
+
+use std::collections::HashMap;
 
 use crate::PeekValue;
 
 pub struct MixNode {
 	pub base: MixBase,
-	// pub variants: HashMap<MixKey, MixValue>,
+	pub variants: Option<MixVariants>,
 }
 
 impl Parse for MixNode {
 	fn parse(input: ParseStream) -> syn::Result<Self> {
-		let mut base: Option<MixBase> = None;
+		let mut base = None;
+		let mut variants = None;
 
 		while let Ok(el_type) = input.parse() {
 			match el_type {
@@ -23,13 +26,21 @@ impl Parse for MixNode {
 
 					base = Some(input.parse()?)
 				}
+				MixElementType::Variants => {
+					variants
+						.is_none()
+						.ok_or_else(|| input.error("Property `variants` already assigned"))?;
+
+					variants = Some(input.parse()?)
+				}
 			}
 
-			let _comma: Token![,] = input.parse()?;
+			input.parse::<Token![,]>().ok();
 		}
 
 		Ok(MixNode {
 			base: base.ok_or_else(|| input.error("Property `base` needed to build Mix"))?,
+			variants,
 		})
 	}
 }
@@ -67,14 +78,63 @@ impl PeekValue<()> for MixBase {
 	}
 }
 
+pub struct MixVariants(HashMap<MixKey, MixValue>);
+
+impl Parse for MixVariants {
+	fn parse(input: ParseStream) -> syn::Result<Self> {
+		input
+			.parse::<Ident>()?
+			.to_string()
+			.eq("variants")
+			.ok_or_else(|| input.error("Expected variants identifier"))?;
+
+		let _q: Token![:] = input.parse()?;
+
+		input
+			.cursor()
+			.group(proc_macro2::Delimiter::Brace)
+			.is_some()
+			.ok_or_else(|| input.error("Block expected"))?;
+
+		let mut map = HashMap::new();
+
+		let content;
+		let brace = braced!(content in input);
+
+		while let ident = content.parse::<Ident>()? {
+			let _q: Token![:] = content.parse()?;
+
+			let val = content.parse()?;
+
+			map.insert(ident, val);
+
+			if content.parse::<Token![,]>().is_err() {
+				break;
+			}
+		}
+
+		Ok(Self(map))
+	}
+}
+
+impl PeekValue<()> for MixVariants {
+	fn peek(cursor: Cursor) -> Option<()> {
+		let (ident, _) = cursor.ident()?;
+		(ident == "variants").as_option()
+	}
+}
+
 pub enum MixElementType {
 	Base,
+	Variants,
 }
 
 impl Parse for MixElementType {
 	fn parse(input: ParseStream) -> syn::Result<Self> {
 		if MixBase::peek(input.cursor()).is_some() {
 			Ok(MixElementType::Base)
+		} else if MixVariants::peek(input.cursor()).is_some() {
+			Ok(MixElementType::Variants)
 		} else {
 			Err(input.error(format!(
 				"Unexpected token: `{}`",
@@ -84,19 +144,25 @@ impl Parse for MixElementType {
 	}
 }
 
-pub type MixKey = Box<Ident>;
+pub type MixKey = Ident;
 
 pub enum MixValue {
-	Literal(Lit),
-	Format(MixFormat),
+	Literal(LitStr),
+	// Format(MixFormat),
+}
+
+impl Parse for MixValue {
+	fn parse(input: ParseStream) -> syn::Result<Self> {
+		Ok(Self::Literal(input.parse()?))
+	}
 }
 
 pub struct MixFormat {
-	pub lit: Lit,
+	pub lit: LitStr,
 	pub args: Vec<MixFormatArg>,
 }
 
 pub enum MixFormatArg {
-	Literal(Lit),
+	Literal(LitStr),
 	Expression(Expr),
 }
