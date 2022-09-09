a19a2c0b9aed467163102ee4e56197c0feb890e8
itsezc <chirub@foretag.co>
2022-09-10 00:33:19 +0100

    feat(package): defaults

---
+++ packages/defaults/README.md
@@ -0,0 +1,5 @@
+# Defaults
+
+Defaults for your design language.
+
+- MasterCSS rootSize for 10em
---
+++ packages/defaults/source/index.ts
@@ -0,0 +1,9 @@
+/**
+ * MasterCSS Configuration
+ * Docs: https://docs.master.co/css/customization
+ */
+import { Style, init } from '@master/css';
+
+Style.rootSize = 10;
+
+init();
